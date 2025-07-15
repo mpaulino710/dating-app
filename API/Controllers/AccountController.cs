f33c368c165fbf08b9fa28f0d1f8c6b811720856
mpaulino710 <mpaulino710@gmail.com>
2025-07-14 23:47:52 -0400

    End of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
+{
+    [HttpPost("register")] // localhost:5001/api/account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        // Check if the email already exists
+        if (await EmailExists(registerDto.Email))
+        {
+            return BadRequest("Email already exists");
+        }
+
+        // generate aleatory salt and hash the password
+        using var hmac = new HMACSHA512();
+
+        // Create new user
+        var user = new AppUser
+        {
+            DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)), // Placeholder for password hash
+            PasswordSalt = hmac.Key  // Placeholder for password salt
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user.ToDto(tokenService); // Convert the user to UserDto and return it
+    }
+
+    [HttpPost("login")] // localhost:5001/api/account/login
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        // Check if the user exists
+        var user = await context.Users.SingleOrDefaultAsync(x => x.Email.ToLower() == loginDto.Email.ToLower());
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid email or password");
+        }
+
+        // Verify the password
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid email or password");
+            }
+        }
+
+        return user.ToDto(tokenService); // Convert the user to UserDto and return it
+    }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +16,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("TokenKey is not configured. - Program.cs");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -19,6 +37,9 @@ app.UseCors(x => x
     .AllowAnyMethod()
     .AllowAnyHeader()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication();    // Add authentication middleware
+app.UseAuthorization();     // Add authorization middleware
 
 app.MapControllers();
 
